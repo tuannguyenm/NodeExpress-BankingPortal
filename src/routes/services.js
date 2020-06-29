21687fd09c7a50900e34994dbb76be0a9f959c47
tuannguyenm <tuan.nguyenminh@nashtechglobal.com>
2020-06-29 10:02:12 +0700

    Using the Express Router

---
+++ src/routes/services.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const { accounts, writeJSON } = require('../data');
+
+const router = express.Router();
+
+router.get('/transfer', (req, res) => {
+    res.render('transfer', {});
+});
+
+router.post('/transfer', (req, res) => {
+    accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
+    accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10);
+    writeJSON();
+    res.render('transfer', { message: 'Transfer Completed' })
+});
+
+router.get('/payment', (req, res) => {
+    res.render('payment', { account: accounts.credit });
+});
+
+router.post('/payment', (req, res) => {
+    accounts.credit.balance -= req.body.amount;
+    accounts.credit.available += parseInt(req.body.amount, 10);
+    writeJSON();
+    res.render('payment', { message: 'Payment Successful', account: accounts.credit });
+});
+
+module.exports = router;
